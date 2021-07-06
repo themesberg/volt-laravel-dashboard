fb40781a2563844c5a913f81ec5f965a6aee0bdb
Ovidiu Stanc <ovidiu.stanc@updivision.com>
2021-07-06 10:54:50 +0300

    bugfixing

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,6 +17,7 @@ class UserSeeder extends Seeder
     {
         DB::table("users")->insert([
             'first_name' => 'Admin',
+            'last_name' => 'User',
             'email' => 'admin@volt.com',
             'password' => Hash::make('secret'),
         ]);
