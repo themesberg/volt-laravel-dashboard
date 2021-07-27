aea1700ce77f4df51af0fc9c45128bfa6e38255e
Ovidiu Stanc <ovidiu.stanc@updivision.com>
2021-07-27 15:06:41 +0300

    bugfixing

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -22,6 +22,18 @@
         <meta  property="og:description" content="Volt Laravel Admin Dashboard features dozens of UI components and a Laravel backend with Livewire & Alpine.js"></meta>
         <meta  property="og:site_name" content="Themesberg"></meta>
     @endif
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="../../assets/img/favicon/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="../../assets/img/favicon/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="../../assets/img/favicon/favicon-16x16.png" sizes="16x16" type="image/png">
+
+    <link rel="mask-icon" href="../../assets/img/favicon/safari-pinned-tab.svg" color="#563d7c">
+    <link rel="icon" href="../../assets/img/favicon/favicon.ico">
+    <meta name="msapplication-config" content="../../assets/img/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#563d7c">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    
     <!-- Apex Charts -->
     <link type="text/css" href="/vendor/apexcharts/apexcharts.css" rel="stylesheet">
     
